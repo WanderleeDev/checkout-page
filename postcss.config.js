a112f73c4a0dbab582803ca085bcbc0c32a736ff
Wanderlee Max <wanderleedev@gmail.com>
2023-06-10 13:11:22 -0500

    Base de proyecto lista -postCSS configurado

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'postcss-nested': {},
+    'postcss-extend-rule': {},
+    'autoprefixer': {},
+    'cssnano': {},
+    'postcss-preset-env': {
+      stage: 3
+    }
+  }
+}
